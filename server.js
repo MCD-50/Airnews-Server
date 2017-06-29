59e2afaf85c7e96dac248c153dd03fb9ae8f502e
mcd-50 <ayush.as.shukla@gmail.com>
2017-06-29 21:12:41 +0530

    Fix

--- server.js
+++ server.js
@@ -38,12 +38,15 @@ app.post('/scrape', function (req, res) {
 	//build query
 	const pagenum = "?pagenum=" + body.page_number || "?pagenum=1";
 	const action = ACTION;
-	const search_string = "&search_string=" + body.search_string;
+	let search_string = "&search_string=" + body.search_string || "top news";
+	const selected_country = body.selected_country || "india";
+	if (!search_string.includes(selected_country)) {
+		search_string = search_string + selected_country;
+	}
 	const language_id = "&language_id=" + get_lang_id(body.language_name)
 
 	//request query
 	const request_url = BASE + pagenum + action + search_string + language_id;
-
 	//const request_url = BASE + '?pagenum=1&action=search&results_type=news&sort_type=-pub-datetime&search_string=india&language_id=29';
 
 	request(request_url, (error, response, html) => {
@@ -52,17 +55,19 @@ app.post('/scrape', function (req, res) {
 		if (!error && response.statusCode == 200) {
 			// Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
 			const $html = cheerio.load(response.body);
-			
-			$html("div.news-story").each( function (i, e){
+
+			$html("div.news-story").each(function (i, e) {
 				const title = $html(this).children('div.title').children('a.healine').children().text()
 				const author = $html(this).children('div.title').next().text();
-				const published_at =  $html(this).children('div.summary').prev().text();
+				const published_at = $html(this).children('div.summary').prev().text();
 				const description = $html(this).children('div.summary').children().text();
 				const url = $html(this).children('div.title').next().attr('href')
-				items.push({title : title, author : author, 
-					published_at : published_at, 
-					description : description,
-					url : url});
+				items.push({
+					title: title, author: author,
+					published_at: published_at,
+					description: description,
+					url: url
+				});
 			});
 
 			res.json(prepare_response(items));
@@ -74,8 +79,8 @@ app.post('/scrape', function (req, res) {
 })
 
 const get_lang_id = (lang) => {
-	if(!lang) return 1
-	lang =lang.toLowerCase().trim();
+	if (!lang) return 1
+	lang = lang.toLowerCase().trim();
 	const id = language_id[lang] || 1
 	return id;
 }
